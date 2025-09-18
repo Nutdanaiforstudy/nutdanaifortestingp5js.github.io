fc3196888594c2c151c7aee2f9169c176a52f2a5
Nutdanaiforstudy <nutdanaiforstudy2@gmail.com>
2025-09-18 19:02:19 +0700

    Update Test.js

--- Javascript/Test.js
+++ Javascript/Test.js
@@ -69,7 +69,8 @@ class Sprite {
 
 // --- Unit class ---
 class Unit {
-  constructor(x, y, scale = 1.5) {
+  constructor(x, y, scale = 1.5, label = "P") {
+    this.label = label;
     this.health = 100;
     this.maxHealth = 100;
     this.speed = 5;
@@ -92,6 +93,7 @@ class Unit {
   play() {
     this.currentSprite.play();
     this.drawHealthBar();
+    this.drawLabel();
     this.handleAttackEnd();
   }
 
@@ -103,7 +105,7 @@ class Unit {
     let healthRatio = this.health / this.maxHealth;
 
     let x = this.currentSprite.posX + 10;
-    let y = this.currentSprite.posY - 20;
+    let y = this.currentSprite.posY - 25;
 
     fill(255, 0, 0);
     rect(x, y, barWidth, barHeight);
@@ -113,24 +115,33 @@ class Unit {
     pop();
   }
 
-  handleMovement() {
+  drawLabel() {
+    push();
+    fill(0);
+    textAlign(CENTER);
+    textSize(14);
+    text(this.label, this.currentSprite.posX + 35, this.currentSprite.posY - 35);
+    pop();
+  }
+
+  handleMovement(left, right, up, down) {
     let moving = false;
 
-    if (keyIsDown(65)) { // A
+    if (keyIsDown(left)) { // Left
       this.run.isFlipX = true;
       this.run.posX -= this.speed;
       moving = true;
     }
-    if (keyIsDown(68)) { // D
+    if (keyIsDown(right)) { // Right
       this.run.isFlipX = false;
       this.run.posX += this.speed;
       moving = true;
     }
-    if (keyIsDown(87)) { // W
+    if (keyIsDown(up)) { // Up
       this.run.posY -= this.speed;
       moving = true;
     }
-    if (keyIsDown(83)) { // S
+    if (keyIsDown(down)) { // Down
       this.run.posY += this.speed;
       moving = true;
     }
@@ -186,23 +197,28 @@ class Unit {
 }
 
 // --- Global ---
-let warrior;
+let warrior1, warrior2;
 
 function preload() {
-  warrior = new Unit(100, 100, 1.5);
-  warrior.preload();
+  warrior1 = new Unit(100, 100, 1.5, "P1");
+  warrior2 = new Unit(300, 300, 1.5, "P2");
+
+  warrior1.preload();
+  warrior2.preload();
 }
 
 function setup() {
   createCanvas(512, 512);
   frameRate(6);
 
-  // --- Attack button ---
-  const attackButton = createButton('Attack');
-  attackButton.position(10, 530);
-  attackButton.style('font-size', '16px');
-  attackButton.style('padding', '8px 16px');
-  attackButton.mousePressed(() => warrior.triggerAttack());
+  // --- Attack buttons ---
+  const attackButton1 = createButton('P1 Attack');
+  attackButton1.position(10, 530);
+  attackButton1.mousePressed(() => warrior1.triggerAttack());
+
+  const attackButton2 = createButton('P2 Attack');
+  attackButton2.position(120, 530);
+  attackButton2.mousePressed(() => warrior2.triggerAttack());
 
   // Autofocus
   canvas = document.querySelector('canvas');
@@ -212,17 +228,28 @@ function setup() {
 
 function draw() {
   background(255, 204, 0);
-  warrior.handleMovement();
-  warrior.play();
+
+  // P1 movement (WASD)
+  warrior1.handleMovement(65, 68, 87, 83); // A, D, W, S
+
+  // P2 movement (UHJK)
+  warrior2.handleMovement(72, 75, 85, 74); // H, K, U, J
+
+  warrior1.play();
+  warrior2.play();
 }
 
 function keyPressed() {
   if (keyCode === 32) return false; // stop page scroll
 
-  if (key === 'f' || key === 'F') warrior.currentSprite.isFlipX = !warrior.currentSprite.isFlipX;
-  if (key === 'c' || key === 'C') warrior.currentSprite.isStop = !warrior.currentSprite.isStop;
-  if (key === 'l' || key === 'L') warrior.currentSprite.isLoop = !warrior.currentSprite.isLoop;
-  if (key === 'x' || key === 'X' || keyCode === 32) warrior.triggerAttack();
+  // --- P1 Controls ---
+  if (key === 'f' || key === 'F') warrior1.currentSprite.isFlipX = !warrior1.currentSprite.isFlipX;
+  if (key === 'c' || key === 'C') warrior1.currentSprite.isStop = !warrior1.currentSprite.isStop;
+  if (key === 'l' || key === 'L') warrior1.currentSprite.isLoop = !warrior1.currentSprite.isLoop;
+  if (key === 'x' || key === 'X') warrior1.triggerAttack();
+
+  // --- P2 Controls ---
+  if (key === 'm' || key === 'M') warrior2.triggerAttack();
 }
 
 function keyReleased() {
