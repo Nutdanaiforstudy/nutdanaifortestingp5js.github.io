726c6b2e802f43c064eb262d6981436989d31fe4
Nutdanaiforstudy <nutdanaiforstudy2@gmail.com>
2025-09-18 18:53:16 +0700

    Update Test.js

--- Javascript/Test.js
+++ Javascript/Test.js
@@ -1,16 +1,13 @@
-// --- Sprite class ---
+// --- Sprite class (animation handler) ---
 class Sprite {
-  constructor(characterFolder, actionFolder, fileName, fileExt, totalSize, posX, posY, scale) {
+  constructor(characterFolder, actionFolder, fileName, fileExt, totalSize, scale) {
     this.characterFolder = characterFolder;
     this.actionFolder = actionFolder;
     this.fileName = fileName;
     this.fileExt = fileExt;
     this.totalSize = totalSize;
 
-    this.posX = posX;
-    this.posY = posY;
     this.spriteScale = scale;
-
     this.images = [];
     this.currentIndex = 0;
     this.isStop = false;
@@ -25,9 +22,9 @@ class Sprite {
     }
   }
 
-  render() {
+  render(x, y) {
     push();
-    translate(this.posX, this.posY);
+    translate(x, y);
 
     if (this.isFlipX) {
       scale(-this.spriteScale, this.spriteScale);
@@ -40,7 +37,7 @@ class Sprite {
     pop();
   }
 
-  renderNext() {
+  renderNext(x, y) {
     if (!this.isStop) {
       this.currentIndex++;
       if (this.currentIndex >= this.totalSize) {
@@ -51,176 +48,185 @@ class Sprite {
         }
       }
     }
-    this.render();
+    this.render(x, y);
   }
 
-  play() {
+  play(x, y) {
     if (this.isLoop) {
-      this.renderNext();
+      this.renderNext(x, y);
     } else {
       if (this.currentIndex === this.totalSize - 1) {
-        this.render();
+        this.render(x, y);
       } else {
-        this.renderNext();
+        this.renderNext(x, y);
       }
     }
   }
 }
 
+// --- Unit class (character with animations + health) ---
+class Unit {
+  constructor(x, y, scale = 1.5) {
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+
+    // Health system
+    this.maxHealth = 100;
+    this.currentHealth = 100;
+
+    // Movement
+    this.speed = 5;
+
+    // States
+    this.state = "idle"; // idle, run, attack
+    this.attackQueue = 0;
+
+    // Sprites
+    this.idle = new Sprite("Warrior", "idle", "Warrior_Idle_", "png", 6, this.scale);
+    this.run = new Sprite("Warrior", "Run", "Warrior_Run_", "png", 8, this.scale);
+    this.attack = new Sprite("Warrior", "Attack", "Warrior_Attack_", "png", 12, this.scale);
+    this.currentSprite = this.idle;
+  }
+
+  preload() {
+    this.idle.preload();
+    this.run.preload();
+    this.attack.preload();
+  }
+
+  update() {
+    let moving = false;
+
+    // Movement WASD
+    if (keyIsDown(65)) { // A
+      this.run.isFlipX = true;
+      this.x -= this.speed;
+      moving = true;
+    }
+    if (keyIsDown(68)) { // D
+      this.run.isFlipX = false;
+      this.x += this.speed;
+      moving = true;
+    }
+    if (keyIsDown(87)) { // W
+      this.y -= this.speed;
+      moving = true;
+    }
+    if (keyIsDown(83)) { // S
+      this.y += this.speed;
+      moving = true;
+    }
+
+    // Pac-Man style wrapping
+    let w = this.run.images[0].width * this.scale;
+    let h = this.run.images[0].height * this.scale;
+
+    if (this.x > width) this.x = -w;
+    if (this.x + w < 0) this.x = width;
+    if (this.y > height) this.y = -h;
+    if (this.y + h < 0) this.y = height;
+
+    // Update state
+    if (this.state !== "attack") {
+      if (moving) {
+        this.state = "run";
+        this.currentSprite = this.run;
+      } else {
+        this.state = "idle";
+        this.currentSprite = this.idle;
+      }
+    }
+
+    // Handle attack animation
+    if (this.state === "attack") {
+      if (this.attack.currentIndex === this.attack.totalSize - 1) {
+        if (this.attackQueue > 1) {
+          this.attackQueue--;
+          this.attack.currentIndex = 0;
+        } else {
+          this.state = "idle";
+          this.currentSprite = this.idle;
+        }
+      }
+    }
+  }
+
+  draw() {
+    // Draw sprite
+    this.currentSprite.play(this.x, this.y);
+
+    // Draw health bar
+    this.drawHealthBar();
+  }
+
+  drawHealthBar() {
+    let barWidth = 80;
+    let barHeight = 10;
+    let healthPercent = this.currentHealth / this.maxHealth;
+
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    rect(this.x, this.y - 20, barWidth, barHeight); // background (red)
+    fill(0, 255, 0);
+    rect(this.x, this.y - 20, barWidth * healthPercent, barHeight); // foreground (green)
+    pop();
+  }
+
+  takeDamage(amount) {
+    this.currentHealth -= amount;
+    if (this.currentHealth < 0) this.currentHealth = 0;
+  }
+
+  heal(amount) {
+    this.currentHealth += amount;
+    if (this.currentHealth > this.maxHealth) this.currentHealth = this.maxHealth;
+  }
+
+  triggerAttack() {
+    if (this.state !== "attack") {
+      this.state = "attack";
+      this.currentSprite = this.attack;
+      this.attack.currentIndex = 0;
+    }
+    this.attackQueue++;
+  }
+}
+
 // --- Global variables ---
-let warriorIdle;
-let warriorAttack;
-let warriorRun;
-let currentSprite;
-let attackQueue = 0; // Counts how many attack requests are queued
-let speed = 5;       // Movement speed
+let warrior;
 
 function preload() {
-  warriorIdle = new Sprite("Warrior", "idle", "Warrior_Idle_", "png", 6, 100, 100, 1.5);
-  warriorIdle.preload();
-
-  warriorAttack = new Sprite("Warrior", "Attack", "Warrior_Attack_", "png", 12, 100, 100, 1.5);
-  warriorAttack.preload();
-
-  warriorRun = new Sprite("Warrior", "Run", "Warrior_Run_", "png", 8, 100, 100, 1.5);
-  warriorRun.preload();
+  warrior = new Unit(100, 100, 1.5);
+  warrior.preload();
 }
 
 function setup() {
   createCanvas(512, 512);
   frameRate(6);
-  currentSprite = warriorIdle;
 
-  // --- Create the Attack button ---
+  // Attack button
   const attackButton = createButton('Attack');
   attackButton.position(10, 530);
-  attackButton.style('font-size', '16px');
-  attackButton.style('padding', '8px 16px');
-
   attackButton.mousePressed(() => {
-    triggerAttack();
+    warrior.triggerAttack();
   });
-
-  // Autofocus canvas so keys work without clicking
-  canvas = document.querySelector('canvas');
-  canvas.setAttribute('tabindex', '0');
-  canvas.focus();
 }
 
 function draw() {
   background(255, 204, 0);
-
-  handleMovement();
-
-  currentSprite.play();
-
-  // If current animation is the attack
-  if (currentSprite === warriorAttack) {
-    if (warriorAttack.currentIndex === warriorAttack.totalSize - 1) {
-      if (attackQueue > 1) {
-        attackQueue--;
-        warriorAttack.currentIndex = 0;
-      } else if (attackQueue === 1) {
-        attackQueue = 0;
-      } else {
-        currentSprite = warriorIdle;
-        warriorIdle.currentIndex = 0;
-      }
-    }
-  }
+  warrior.update();
+  warrior.draw();
 }
 
-// --- Movement control (WASD keys) ---
-function handleMovement() {
-  let moving = false;
-
-  if (keyIsDown(65)) { // A = left
-    warriorRun.isFlipX = true;
-    warriorRun.posX -= speed;
-    moving = true;
-  }
-  if (keyIsDown(68)) { // D = right
-    warriorRun.isFlipX = false;
-    warriorRun.posX += speed;
-    moving = true;
-  }
-  if (keyIsDown(87)) { // W = up
-    warriorRun.posY -= speed;
-    moving = true;
-  }
-  if (keyIsDown(83)) { // S = down
-    warriorRun.posY += speed;
-    moving = true;
-  }
-
-  // --- Pac-Man style screen wrapping ---
-  let w = warriorRun.images[0].width * warriorRun.spriteScale;
-  let h = warriorRun.images[0].height * warriorRun.spriteScale;
-
-  if (warriorRun.posX > width) {
-    warriorRun.posX = -w;
-  }
-  if (warriorRun.posX + w < 0) {
-    warriorRun.posX = width;
-  }
-  if (warriorRun.posY > height) {
-    warriorRun.posY = -h;
-  }
-  if (warriorRun.posY + h < 0) {
-    warriorRun.posY = height;
-  }
-
-  if (currentSprite !== warriorAttack) {
-    if (moving) {
-      warriorRun.posX = currentSprite.posX;
-      warriorRun.posY = currentSprite.posY;
-      currentSprite = warriorRun;
-    } else if (currentSprite === warriorRun && !moving) {
-      warriorIdle.posX = warriorRun.posX;
-      warriorIdle.posY = warriorRun.posY;
-      currentSprite = warriorIdle;
-    }
-  }
-}
-
-// --- Attack trigger (button + key) ---
-function triggerAttack() {
-  if (currentSprite === warriorIdle || currentSprite === warriorRun) {
-    warriorAttack.currentIndex = 0;
-    warriorAttack.isLoop = false;
-    warriorAttack.posX = currentSprite.posX;
-    warriorAttack.posY = currentSprite.posY;
-    currentSprite = warriorAttack;
-  }
-  attackQueue++;
-}
-
-// --- Key controls ---
 function keyPressed() {
-  // Prevent page scrolling on space
   if (keyCode === 32) {
-    return false; // stop browser default
+    warrior.triggerAttack();
+    return false; // prevent page scroll
   }
 
-  if (key === 'f' || key === 'F') {
-    currentSprite.isFlipX = !currentSprite.isFlipX;
-  }
-
-  if (key === 'c' || key === 'C') {   // <-- Changed stop toggle to C
-    currentSprite.isStop = !currentSprite.isStop;
-  }
-
-  if (key === 'l' || key === 'L') {
-    currentSprite.isLoop = !currentSprite.isLoop;
-  }
-
-  if (key === 'x' || key === 'X' || keyCode === 32) { // X or Spacebar
-    triggerAttack();
-  }
-}
-
-function keyReleased() {
-  if (keyCode === 32) {
-    return false;
+  if (key === 'c' || key === 'C') {
+    warrior.currentSprite.isStop = !warrior.currentSprite.isStop;
   }
 }
